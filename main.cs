6f5f4baa522f24615eb612213c6534475e579b22
afterpartyGarage <vladislavmartyanov444@gmail.com>
2023-03-18 17:19:43 +0500

    17 (с правками + завершенное)

--- main.cs
+++ main.cs
@@ -8,29 +8,47 @@ public enum CircleType
 
 class Program
 {
-  public static double SphereArea(double radius) => 4 * Math.PI * Math.Pow(radius, 2);
-  public static double SphericalCapArea(double radius) => 4.0 / 3.0 * Math.PI * Math.Pow(radius, 3);
+  public static double RingArea(double outerRadius, double innerRadius)
+  {
+    if (outerRadius <= 0 || innerRadius <= 0)
+    {
+      throw new ArgumentException("Radius should be positive.");
+    }
 
+    if (innerRadius >= outerRadius)
+    {
+      throw new ArgumentException("Inner radius should be less than outer radius.");
+    }
+    return Math.PI * (outerRadius * outerRadius - innerRadius * innerRadius);
+  }
+  public static double CalculateRingVolume(double outerRadius, double innerRadius, double height)
+  {
+    return Math.PI * height * (outerRadius * outerRadius - innerRadius * innerRadius);
+  }
+
+  public static double CalculateRingSurfaceArea(double outerRadius, double innerRadius, double height)
+  {
+    double outerSurfaceArea = 2 * Math.PI * outerRadius * height;
+    double innerSurfaceArea = 2 * Math.PI * innerRadius * height;
+    double topArea = Math.PI * outerRadius * outerRadius;
+    double bottomArea = Math.PI * innerRadius * innerRadius;
+
+    return outerSurfaceArea + innerSurfaceArea + topArea + bottomArea;
+  }
   public static void Main(string[] args)
   {
-    Console.WriteLine("Задача 18\n");
-    Console.Write("Введите радиус: ");
-    double r = double.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задача 11\n");
+    Console.Write("Введите радиус внешнего круга: ");
+    double outerRad = double.Parse(Console.ReadLine()!);
+    Console.Write("Введите радиус внутреннего круга: ");
+    double innerRad = double.Parse(Console.ReadLine()!);
+    Console.Write("Укажите высоту кольца: ");
+    double height = double.Parse(Console.ReadLine()!);
+    Console.WriteLine($"Площадь кольца равна: {RingArea(outerRad, innerRad)}");
+    Console.WriteLine($"Объём кольца равен: {CalculateRingVolume(outerRad, innerRad, height)}");
+    Console.WriteLine($"Площадь поверхности кольца равна: {CalculateRingSurfaceArea(outerRad, innerRad, height)}");
+    
 
-    Console.WriteLine("Выберите операцию:\n\t[1] - Площадь сферы\n\t[2] - Площадь шара");
-    string userInput = Console.ReadLine();
-    switch (userInput)
-    {
-      case "1":
-        Console.WriteLine($"Площадь сферы равна: {SphereArea(r)}");
-        break;
-      case "2":
-        Console.WriteLine($"Площадь окружности равна: {SphericalCapArea(r)}");
-        break;
-      default:
-        Console.WriteLine("Такой операции нет!");
-        break;
-    }
   }
 }
 
@@ -89,6 +107,27 @@ Console.WriteLine($"Диагональ квадрата равен: {CalculateDi
     double r = double.Parse(Console.ReadLine()!);
     Console.WriteLine($"Площадь сферы равна: {SphereArea(r)}");
     Console.WriteLine($"Площадь окружности равна: {SphericalCapArea(r)}");
+
+
+    //18 (c правками)
+        Console.WriteLine("Задача 18\n");
+    Console.Write("Введите радиус: ");
+    double r = double.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Выберите операцию:\n\t[1] - Площадь сферы\n\t[2] - Площадь шара");
+    string userInput = Console.ReadLine();
+    switch (userInput)
+    {
+      case "1":
+        Console.WriteLine($"Площадь сферы равна: {SphereArea(r)}");
+        break;
+      case "2":
+        Console.WriteLine($"Площадь окружности равна: {SphericalCapArea(r)}");
+        break;
+      default:
+        Console.WriteLine("Такой операции нет!");
+        break;
+    }
 */
 /*
     //17
